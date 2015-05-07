2318d9008cdf9bae692edcc4fe095347ad8413e8
UsrNameu1 <tea_room_1028@yahoo.co.jp>
2015-05-07 20:51:20 +0900

    Add basic wireframe

---
+++ RadioButtonCheckboxTableView/User.swift
@@ -0,0 +1,20 @@
+//
+//  User.swift
+//  RadioButtonCheckboxTableView
+//
+//  Created by adachi.yuichi on 2015/05/07.
+//  Copyright (c) 2015年 yad. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    
+    let name: String
+    
+    let gender: Gender
+    
+    let hasLicense: Bool
+    
+    let createDate = NSDate()
+}
